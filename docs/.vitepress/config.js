74b0d03bc730bf7a7bd0acaee4ba2fbed9f35d9b
Valgeir Bjornsson <valgeirb@nextgl.com>
2021-06-12 22:31:09 +0000

    Test

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,7 +1,7 @@
 const { description } = require("../../package");
 
 module.exports = {
-  // base: "/vue-popper/",
+  base: "/vue-popper/",
   title: "Vue Popper",
   description: description,
   /**
