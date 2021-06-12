4d657442b89f9e59d16bb7e217686679e5993cfd
Valgeir Bjornsson <valgeirb@nextgl.com>
2021-06-12 22:49:33 +0000

    Updates

---
+++ assets/guide_what-is-vue-popper.md.76b4f4de.lean.js
@@ -0,0 +1,1 @@
+import{o as e,c as r,a}from"./app.ec7e94e9.js";const t='{"title":"What is Vue Popper","description":"","frontmatter":{},"headers":[{"level":2,"title":"Features","slug":"features"}],"relativePath":"guide/what-is-vue-popper.md","lastUpdated":1623446598887}',p={},i=a('',4);p.render=function(a,t,p,o,s,l){return e(),r("div",null,[i])};export default p;export{t as __pageData};
